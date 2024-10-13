2fca654fa47694c0cb35cfb7f2c677c14a270896
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-13 16:37:22 +0200

    read_posts version 0.2 reads from csv

--- read_posts.py
+++ read_posts.py
@@ -1,15 +1,25 @@
 ######################################################################################
 # File: read_posts.py
-# Description: open files and read cost data from file
-# Version: 0.1 October 13th 2024
+# Description: open files and read cost data from csv file
+# Version: 0.2 October 13th 2024
 # Author: Kjell Inge Tomren, kitomren@gmail.com
-# Project likv with Python
+# Project: likv with Python
 #
 #
 ######################################################################################
-with open("simple_cost_budget.data", mode="r") as file:
-  for (line) in file:
-        print( line )
+import csv 
 
-file.close()
+with open('simple_cost_budget.data') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'First line, {": ".join(row)}')
+            line_count += 1
+        else:
+            print(f'Cost text: {row[0]},\t\t\tAmount: {row[1]}')
+            line_count += 1
 
+    print(f'Processed {line_count} lines.')
+
+csv_file.close()
---
+++ template.py
@@ -0,0 +1,10 @@
+######################################################################################
+# File: templ.py
+# Description: write descripion!
+# Version: 0.1 October 13th 2024
+# Author: Kjell Inge Tomren, kitomren@gmail.com
+# Project: likv with Python
+#
+#
+######################################################################################
+from sys import argv
