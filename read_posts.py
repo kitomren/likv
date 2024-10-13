3a361c7f774e20d2817537d9c50ada9e7d9ea11e
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-13 19:34:30 +0200

    read_posts version 0.3 converts string to decimal

--- read_posts.py
+++ read_posts.py
@@ -1,7 +1,7 @@
 ######################################################################################
 # File: read_posts.py
 # Description: open files and read cost data from csv file
-# Version: 0.2 October 13th 2024
+# Version: 0.3 October 13th 2024
 # Author: Kjell Inge Tomren, kitomren@gmail.com
 # Project: likv with Python
 #
@@ -17,7 +17,8 @@ with open('simple_cost_budget.data') as csv_file:
             print(f'First line, {": ".join(row)}')
             line_count += 1
         else:
-            print(f'Cost text: {row[0]},\t\t\tAmount: {row[1]}')
+            amount = float(row[1])
+            print(f'Cost text: {row[0]}, Amount: {amount}')
             line_count += 1
 
     print(f'Processed {line_count} lines.')
