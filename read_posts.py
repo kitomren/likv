6d490804b38f626d9c4bacfd9a75f695286b1ee9
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-19 21:28:55 +0200

    read_posts.py v0.5 reads dates and converts to internal dates

--- read_posts.py
+++ read_posts.py
@@ -1,13 +1,14 @@
 ######################################################################################
 # File: read_posts.py
 # Description: open files and read cost data from csv file
-# Version: 0.4 October 16th 2024
+# Version: 0.5 October 19th 2024
 # Author: Kjell Inge Tomren, kitomren@gmail.com
 # Project: likv with Python
 #
 #
 ######################################################################################
 import csv 
+from datetime import datetime
 
 with open('simple_cost_budget.data') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
@@ -17,9 +18,12 @@ with open('simple_cost_budget.data') as csv_file:
             print(f'Comment line, {": ".join(row)}')
             line_count += 1
         else:
-            day = int(row[0])
-            amount = float(row[2])
-            print(f'Day of month: {day} \ttext: {row[1]}, Amount: {amount}')
+            text = row[0]
+            number = int(row[1])
+#            date = convert-to-internal(row[2])
+            time_obj=datetime.strptime(row[2],'%Y/%m/%d')
+            date = time_obj.strftime("%A %B %d %Y")
+            print(f'{text} Antall: {number} Dato: {date}')
             line_count += 1
 
     print(f'Processed {line_count} lines.')
