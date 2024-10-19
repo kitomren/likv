7a55873be8d5778edd3de209f638d9f789f7d805
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-19 17:28:41 +0200

    date_example.py updatd and new format defined and edited in simple_cost_budget.data

--- date_example.py
+++ date_example.py
@@ -1,6 +1,12 @@
 
 from datetime import datetime
 
+# import locale
+# locale.setlocale(locale.LC_ALL, 'nb-NO')
+# note to self: får feilmelding på denne på MacBook Pro. 
+# Vet ikke om det er koden som er feil eller Macen som ikke har support for norsk. 
+# må sjekke dette nærmere.
+
 now = datetime.now() # current date and time
 
 year = now.strftime("%Y")
@@ -15,5 +21,5 @@ print("day:", day)
 time = now.strftime("%H:%M:%S")
 print("time:", time)
 
-date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+date_time = now.strftime("%A%d/%B/%Y, %H:%M:%S")
 print("date and time:",date_time)
