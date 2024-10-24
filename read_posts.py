8b3e1f20a0c6ccce7470668a8097c44f059e044c
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-24 19:18:34 +0200

    read_posts.py v0.7 verified correct read of all post from simple..data

--- read_posts.py
+++ read_posts.py
@@ -1,11 +1,11 @@
 ######################################################################################
 # File: read_posts.py
 # Description: open files and read cost data from csv file
-# Version: 0.5 October 19th 2024
+# Version: 0.7777777 October 24 2024
 # Author: Kjell Inge Tomren, kitomren@gmail.com
 # Project: likv with Python
 #
-#
+# format: text; number; year/month/day; periode_type; periode_delta; fix; amount_in; amount_out
 ######################################################################################
 import csv 
 from datetime import datetime
@@ -14,8 +14,8 @@ with open('simple_cost_budget.data') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0 or line_count == 1:
-            print(f'Comment line, {": ".join(row)}')
+        if row[0][0] == "#":
+#            print(f'Comment line, {row[0][0]}')
             line_count += 1
         else:
             text = row[0]
@@ -23,7 +23,12 @@ with open('simple_cost_budget.data') as csv_file:
 #            date = convert-to-internal(row[2])
             time_obj=datetime.strptime(row[2],'%Y/%m/%d')
             date = time_obj.strftime("%A %B %d %Y")
-            print(f'{text} Antall: {number} Dato: {date}')
+            periode_type = row[3]
+            periode_delta = int(row[4])
+            fix = row[5]
+            amount_in = float(row[6])
+            amount_out = float(row[7])
+            print(f'{text} Antall: {number} Dato: {date} Periode type: {periode_type} pd: {periode_delta} Fix: {fix} Inn: {amount_in} Ut: {amount_out}')
             line_count += 1
 
     print(f'Processed {line_count} lines.')
