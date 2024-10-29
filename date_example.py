68b55418a77199724b17a0458aeed3db27195f57
Kjell Inge Tomren <kitomren@gmail.com>
2024-10-29 19:18:56 +0100

    date_example.py v0.4 with dateutil to do date calculations

--- date_example.py
+++ date_example.py
@@ -1,7 +1,7 @@
 ######################################################################################
 # File: date_example.py
-# Description: write descripion!
-# Version: 0.3 October 28th 2024
+# Description: test of date library functions 
+# Version: 0.4 October 29th 2024
 # Author: Kjell Inge Tomren, kitomren@gmail.com
 # Project: likv with Python
 #
@@ -9,6 +9,7 @@
 ######################################################################################
 # from sys import argv
 import datetime 
+from dateutil.relativedelta import relativedelta
 
 # import locale
 # locale.setlocale(locale.LC_ALL, 'nb-NO')
@@ -18,23 +19,38 @@ import datetime
 
 # class datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
 
-date = datetime.date(2020, 10, 20)
-print(date)
 today = datetime.date.today() # current date 
 print(today)
 
 # next_week = now + 1 week
-# one_week = datetime.datetime.timedelta(weeks=1)
+delta = relativedelta(weeks=1)
+next_date = today + delta
+print(next_date)
+
 # next_week = now + one_week
 # date_time = next_week.strftime("%A%d/%B/%Y, %H:%M:%S")
 # print("next week:",date_time)
 
+# delta = relativedelta(weeks=1,hours=25, day=1, weekday=MO(1))
+# next_date = today + delta
+
 # next_month = now + 1 month
+delta = relativedelta(months=1)
+next_date = today + delta
+print(next_date)
+
+# next_year = now + 1 year
+delta = relativedelta(years=1)
+next_date = today + delta
+print(next_date)
+
+
+# day --> datetime.date(2021, 1, 8)
+
+# deadline --> datetime.date(2021, 2, 8)
 
 #date_time = now.strftime("%A%d/%B/%Y, %H:%M:%S")
 #print("date and time:",date_time)
-
-# next_year = now + 1 year
 
 # date_time = now.strftime("%A%d/%B/%Y, %H:%M:%S")
 # print("date and time:",date_time)
