85d8578e89e97ba064f8c4af5154ed57e0109059
Kjell Inge Tomren <kitomren@gmail.com>
2024-11-05 22:13:27 +0100

    edit_post with comments only

---
+++ edit_posts.py
@@ -0,0 +1,30 @@
+######################################################################################
+# File: edit_post.py
+# Description: module to edit the cost posts 
+# Version: 0.1 November 5th 2024
+# Author: Kjell Inge Tomren, kitomren@gmail.com
+# Project: likv with Python
+#
+#
+######################################################################################
+from sys import argv
+
+# read all post from file and store 
+# 
+# forever
+#   read command
+#   if command = list
+#       counter = 1
+#       for items in the list
+#           print(number, number, date, ......
+#   
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
